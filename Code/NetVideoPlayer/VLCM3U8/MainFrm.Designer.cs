c2b693e9d1717913ee73a4f059cb98cce0bcde7e
Hsuing <thinkxs@outlook.com>
2023-04-24 18:24:45 +0800

    add mqtt listener for video player

--- Code/NetVideoPlayer/VLCM3U8/MainFrm.Designer.cs
+++ Code/NetVideoPlayer/VLCM3U8/MainFrm.Designer.cs
@@ -28,8 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainFrm));
             this.panel1 = new System.Windows.Forms.Panel();
+            this.videoView1 = new LibVLCSharp.WinForms.VideoView();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.videoView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -37,33 +43,60 @@
             this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.panel1.BackColor = System.Drawing.SystemColors.Menu;
-            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Controls.Add(this.videoView1);
+            this.panel1.Location = new System.Drawing.Point(4, 12);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(560, 396);
+            this.panel1.Size = new System.Drawing.Size(776, 555);
             this.panel1.TabIndex = 2;
+            // 
+            // videoView1
+            // 
+            this.videoView1.BackColor = System.Drawing.Color.Black;
+            this.videoView1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("videoView1.BackgroundImage")));
+            this.videoView1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.videoView1.Location = new System.Drawing.Point(8, 3);
+            this.videoView1.MediaPlayer = null;
+            this.videoView1.Name = "videoView1";
+            this.videoView1.Size = new System.Drawing.Size(760, 550);
+            this.videoView1.TabIndex = 0;
+            this.videoView1.Text = "videoView1";
             // 
             // groupBox1
             // 
             this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.groupBox1.BackColor = System.Drawing.SystemColors.Menu;
-            this.groupBox1.Location = new System.Drawing.Point(13, 426);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(4, 573);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(559, 76);
+            this.groupBox1.Size = new System.Drawing.Size(776, 76);
             this.groupBox1.TabIndex = 3;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "信息";
+            this.groupBox1.Text = "Info.";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "当前播放：";
             // 
             // MainFrm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(584, 514);
+            this.ClientSize = new System.Drawing.Size(784, 661);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.panel1);
             this.Name = "MainFrm";
-            this.Text = "HomeGenius_WebPlayer";
+            this.Text = "HomeGenius_WebPlayer_Think";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.videoView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -71,6 +104,8 @@
         #endregion
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.GroupBox groupBox1;
+        private LibVLCSharp.WinForms.VideoView videoView1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
