0ddf9deb03422c8317931adcb91432ec5cb2d505
Hsuing <thinkxs@outlook.com>
2024-05-17 17:40:57 +0800

    update

--- Code/ESP32Clock/ESPAudio/boot.py
+++ Code/ESP32Clock/ESPAudio/boot.py
@@ -19,6 +19,10 @@ def notifyFunc():
         morningdata=[0x7E, 0xFF, 0x06, 0x03, 0x00, 0x00, 0x01, 0xEF]
         noondata=[0x7E, 0xFF, 0x06, 0x03, 0x00, 0x00, 0x02, 0xEF]
         afternoondata=[0x7E, 0xFF, 0x06, 0x03, 0x00, 0x00, 0x03, 0xEF]
+        midvolumndata=[0x7E, 0xFF, 0x06, 0x06, 0x00, 0x00, 0x14, 0xEF]
+        
+        time.sleep(3)
+        uart.write(bytes(midvolumndata))
         key = Pin(12, Pin.IN)
         while True:
             print("GPIO_12:", key.value())
@@ -27,19 +31,21 @@ def notifyFunc():
                 current_time=utime.localtime()
                 
                 if(current_time[3]<8 or (current_time[3]==8 and current_time[4]< 40)):
-                    print("1")
+#                     print("1")
                     uart.write(bytes(morningdata))
-                elif((current_time[3]==11 and current_time[4]> 30) or (current_time[3]==12 and current_time[4]< 30)):
-                    print("2")
+                elif((current_time[3]==11 and current_time[4]>= 30) or (current_time[3]==12 and current_time[4]< 30)):
+#                     print("2")
                     uart.write(bytes(noondata))
-                elif((current_time[3]==17 and current_time[4]> 03) or (current_time[3]>18)):
-                    print("4")
+                elif((current_time[3]==17 and current_time[4]>= 1) or (current_time[3]>=18)):
+#                     print("4")
                     uart.write(bytes(afternoondata))
+                else:
+                    print('invalid')
                 time.sleep(5)
             else:
                 pass
                 #uart.write(bytes(stopdata))
-            time.sleep(1) 
+            time.sleep(0.5) 
     except:
         pass
 
